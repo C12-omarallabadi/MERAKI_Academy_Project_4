5157b2326789e061339e60a70fa4a61737b2b44b
omarallabadi <omarallabadi197@gmail.com>
2024-10-06 04:47:13 -0700

    3-modify role schema

--- backend/models/roleSchema.js
+++ backend/models/roleSchema.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: { type: String, required: true},
-  lastName: { type: String,required: true },
-  age: { type: Number },
-  email: { type: String, required: true ,unique:true },
-  password: { type: Number, required: true },
-  role: { type: mongoose.Schema.Types.ObjectId, ref: "Role"  },
+const roleSchema = new mongoose.Schema({
+  role: { type: String, required: true},
+  permissions:[ { type: String, required: true}],
+
+
 
 });
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", roleSchema);
