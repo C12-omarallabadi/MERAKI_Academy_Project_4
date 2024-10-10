191efcdec318798d752b6f7ccde917729d9c105b
omarallabadi <omarallabadi197@gmail.com>
2024-10-10 14:00:21 -0700

    24-handle delete function

--- frontend/src/components/role 2 interface/UserDashboard.js
+++ frontend/src/components/role 2 interface/UserDashboard.js
@@ -6,14 +6,14 @@ import * as React from "react";
 import { UserContext } from "../../App";
 
 const UserDashboard = () => {
-  
   const user = useContext(UserContext);
   const headers = { Authorization: `Bearer ${user.token}` };
+
   const Navigate = useNavigate();
   const [posts, setPosts] = useState([]);
   useEffect(() => {
     axios
-      .get("http://localhost:5000/posts", { headers})
+      .get("http://localhost:5000/posts", { headers })
       .then((result) => {
         setPosts(result.data);
       })
@@ -28,11 +28,29 @@ const UserDashboard = () => {
           {" "}
           {elem.author.firstName} {elem.author.lastName}
         </h4>
-        <p>{elem.post}</p><br></br>
-        <input placeholder="add comment..."></input><button>add comment</button><br></br>
-        {user.userId===elem.author._id?<button>delete</button>:null}
-      
-        
+        <p>{elem.post}</p>
+        <br></br>
+        <input placeholder="add comment..."></input>
+        <button>add comment</button>
+        <br></br>
+        {user.userId === elem.author._id ? (
+          <button
+            onClick={() => {
+              const id = elem._id;
+              axios
+                .delete(`http://localhost:5000/posts/${id}`, { headers })
+                .then((result) => {
+                  const newPosts = posts.filter((elem) => elem._id != id);
+                  setPosts(newPosts);
+                })
+                .catch((err) => {
+                  console.log(err);
+                });
+            }}
+          >
+            delete
+          </button>
+        ) : null}
       </div>
     );
   });
