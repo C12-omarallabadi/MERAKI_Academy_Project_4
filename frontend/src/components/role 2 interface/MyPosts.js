996c42ed85f36f9ca6f14d57ac48442fbd0d7c56
omarallabadi <omarallabadi197@gmail.com>
2024-10-12 15:48:47 -0700

    40-checkbox2

--- frontend/src/components/role 2 interface/MyPosts.js
+++ frontend/src/components/role 2 interface/MyPosts.js
@@ -1,58 +1,89 @@
-import React from "react"
-import axios from "axios"
-import { useContext,useEffect,useState } from "react"
-import { useNavigate } from "react-router-dom"
-import { UserContext } from "../../App"
+import React from "react";
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../../App";
 import "./myPosts.css";
 
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-const MyPosts =()=>{
-    const [myPosts,setMyPosts]=useState([])
-    const Navigate=useNavigate()
-    const user=useContext(UserContext)
-    const headers = { Authorization: `Bearer ${user.token}` };
-    //////////////////////////////////////////////////////////////
-    useEffect(()=>{
-        axios
-        .get(" http://localhost:5000/posts/myPosts",{headers})
-        .then((result)=>{
-setMyPosts(result.data)        })
-        .catch((err)=>{
-            console.log(err)
-        })
-    },[])
-    //////////////////////////////////////////////////////////////
-    const showMyPosts=myPosts.map((elem,index)=>{
-        return(<div className="myPostContainer" key={index}><div className="myPost" >
-            <h4>{elem.author.fullName}</h4>
-            <div className="postContent"><p>{elem.post}</p></div>
+const MyPosts = () => {
+  const [myPosts, setMyPosts] = useState([]);
+  const [myPostId, setMyPostId] = useState("");
+  const [isBoxShown, setIsBoxShown] = useState("");
+  const Navigate = useNavigate();
+  const user = useContext(UserContext);
+  const headers = { Authorization: `Bearer ${user.token}` };
+  //////////////////////////////////////////////////////////////
+  useEffect(() => {
+    axios
+      .get(" http://localhost:5000/posts/myPosts", { headers })
+      .then((result) => {
+        setMyPosts(result.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  //////////////////////////////////////////////////////////////
+  const showMyPosts = myPosts.map((elem, index) => {
+    return (
+      <div className="myPostContainer" key={index}>
+        <div className="myPost">
+          <h4>{elem.author.fullName}</h4>
+          <div className="postContent">
+            <p>{elem.post}</p>
+          </div>
         </div>
-        <button   onClick={() => {
-              const id = elem._id;
-              axios
-                .delete(`http://localhost:5000/posts/${id}`, { headers })
-                .then((result) => {
-                  const newPosts = myPosts.filter((elem) => elem._id != id);
-                 setMyPosts(newPosts);
-                })
-                .catch((err) => {
-                 
-                  Navigate("/")
-                });
-            }}>delete</button>
+        <button
+          onClick={() => {
+            setIsBoxShown(true);
+            setMyPostId(elem._id);
+          }}
+        >
+          delete
+        </button>
         <button>comment</button>
         <button>update</button>
+      </div>
+    );
+  });
+  //////////////////////////////////////////////////////////////
 
-
-        </div>)
-    })
-    //////////////////////////////////////////////////////////////
- 
-    return (<div className="allPostsContainer">
-    {showMyPosts}
+  return (
+    <div className="allPostsContainer">
+      {showMyPosts}
+      {isBoxShown ? (
+        <div className="checkBox">
+          <h4>are you realy want to delete this post</h4>
+          <button
+            onClick={() => {
+              axios
+                .delete(`http://localhost:5000/posts/${myPostId}`, { headers })
+                .then((result) => {
+                  const newPosts = myPosts.filter(
+                    (elem) => elem._id != myPostId
+                  );
+                  setMyPosts(newPosts);
+                  setIsBoxShown(false);
+                })
+                .catch((err) => {
+                  Navigate("/");
+                });
+            }}
+          >
+            yes
+          </button>
+          <button
+            onClick={() => {
+              setIsBoxShown(false);
+            }}
+          >
+            no
+          </button>
+        </div>
+      ) : null}
     </div>
-      
-    )
-}
-export default MyPosts
+  );
+};
+export default MyPosts;
